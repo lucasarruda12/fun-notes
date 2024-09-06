eb4f4eee6d81f791588db6ac6fd02b525d2cea18
LucasArruda12 <lucas123manoel4@gmail.com>
2024-09-06 14:57:50 -0300

    created livecoding dir and moved files accordingly

--- aulas/Primeiro.hs
+++ aulas/Primeiro.hs
@@ -9,10 +9,5 @@ five = 5
 addFive :: Integer -> Integer
 addFive x = 5 + x
 
-f :: Integer -> Integer
-f x = addFive x + addFive 12
-
-pow :: Integer -> Integer -> Integer
-pow x 0 = 1
-pow x 1 = x
-pow x y = x * pow x (y - 1)
+sempreSix :: Integer -> Integer
+sempreSix x = 6
