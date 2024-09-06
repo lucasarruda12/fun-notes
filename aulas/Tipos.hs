1574cff3dda813e1b25eb0d092300852766d1b5c
LucasArruda12 <lucas123manoel4@gmail.com>
2024-09-06 17:05:05 -0300

    Types e Kinds - 2019-08-12

---
+++ aulas/Tipos.hs
@@ -0,0 +1,32 @@
+module Tipos where
+
+-- Typeclasses: Num; Fractional; Integral;
+--
+-- Primitive types: Char; Int; Integer; Float; Double;
+--
+-- Types: [Char]; (Int, Int); Int -> Int
+
+data Weekday = Mon
+	     | Tue
+	     | Wed
+	     | Thu
+	     | Fri
+	     | Sat
+	     | Sun
+	     deriving (Show)
+
+nextDay :: Weekday -> Weekday
+nextDay Mon = Tue
+nextDay Tue = Wed
+nextDay Wed = Thu
+nextDay Thu = Fri
+nextDay Fri = Sat
+nextDay Sat = Sun
+nextDay Sun = Mon
+
+nextWorkingDay :: Weekday -> Weekday
+nextWorkingDay Mon = Tue
+nextWorkingDay Tue = Wed
+nextWorkingDay Wed = Thu
+nextWorkingDay Thu = Fri
+nextWorkingDay _   = Mon
